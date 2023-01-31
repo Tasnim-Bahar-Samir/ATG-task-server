5555c4ee9630788a7dae6beea1870dc06ae58372
Tasnim-Bahar-Samir <tanimsamir3@gmail.com>
2023-02-01 03:32:57 +0600

    created the api to login user

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 const port = process.env.PORT || 5000;
 const app = express()
 require('dotenv').config()
@@ -13,6 +14,7 @@ app.use(cors())
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.skbfv9j.mongodb.net/?retryWrites=true&w=majority`;
 console.log(uri)
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const userCollection = client.db('dbUser3').collection('users')
 // client.connect(err => {
 //   const collection = client.db("test").collection("devices");
 //   // perform actions on the collection object
@@ -52,6 +54,24 @@ async function run(){
               });
             }
           });
+
+          app.post('/login', async(req,res)=>{
+            const {email,password} = req.body;
+            const user = await userCollection.findOne({email})
+            console.log(user)
+            if(!user){
+              return res.send({success: false, message:"No user found.Please register first."})
+            }
+            if(await bcrypt.compare(password, user.password)){
+              const token = jwt.sign({email},process.env.JWT_SECRETEKEY)
+              if(res.status(201)){
+              return  res.send({ success:true, data: token})
+              }else{
+                return res.send({ success:false, message:'Failed to generate token'})
+               }
+            }
+            res.send({message:"Invalid Password"})
+          })
     }
     catch(err){
         console.log(err.name,err.message)
